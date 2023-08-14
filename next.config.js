00596711679c70d0d1511d59c32e6a4ad4fcdc0d
Vítor Pereira Fontes <vitor@localhost.localdomain>
2023-08-14 11:38:38 -0300

    chore(global): instalando a biblioteca next-PWA
    Instalando a biblioteca next-PWA e adicionando os arquivos de configuração necessários para que o
    PWA funcione corretamente nos  navegadores baseados no Chrome.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  // disable: process.env.NODE_ENV === "development",
+  // register: true,
+  // scope: "/app",
+  // sw: "service-worker.js",
+  //...
+});
+
+// Your Next config is automatically typed!
+module.exports = withPWA({
+  // Your Next.js config
+});

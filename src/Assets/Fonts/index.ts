d68dbde56a6dddda1dcd8430a27b93ed226a0a66
Vítor Pereira Fontes <vitor@localhost.localdomain>
2023-08-17 19:06:05 -0300

    feat(src/assets/fonts): adicionando as fontes que serão utilizadas no projeto
    Criando um arquivo para adicionar e configurar as fontes, provenientes do Google Fonts, que serão
    utilizadas no projeto.

---
+++ src/Assets/Fonts/index.ts
@@ -0,0 +1,11 @@
+import { Raleway } from 'next/font/google'
+
+export const ralewayBold = Raleway({
+    subsets: ['latin'],
+    weight: '700'
+})
+
+export const ralewaySemiBold = Raleway({
+    subsets: ['latin'],
+    weight: '600'
+})

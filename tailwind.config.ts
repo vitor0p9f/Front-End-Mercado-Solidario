92d0747a9a8f517417c893caa9b912ed9cb203d7
Vítor Pereira Fontes <vitor@localhost.localdomain>
2023-08-17 14:27:47 -0300

    chore(global,tailwind.config.ts): instalando a biblioteca Flowbite
    Instalando e configurando a biblioteca Flowbite, para a utilização de componentes prontos e
    estilizados com o Tailwind.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
+    "./node_modules/flowbite-react/**/*.js",
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
@@ -20,6 +21,6 @@ const config: Config = {
       'laptop': '1280',
     }
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 }
 export default config

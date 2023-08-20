e4de2461859e30065b50b2c1e673b1311d5e93cc
rhaissav <rhaissabarbosa4@gmail.com>
2023-08-20 16:39:15 -0300

    feat

---
+++ src/Components/Input/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { InputContainer, InputElement, Label } from './styles'; 
+
+type ComponentProps = {
+  labelTitle: string;
+};
+
+export const InputComponent = ({ labelTitle, ...props }: ComponentProps) => {
+  return (
+    <InputContainer {...props}>
+      <InputElement
+        type="text"
+        id="floating_standard"
+        placeholder=""
+      />
+      <Label
+        htmlFor="floating_standard"
+      >
+        {labelTitle}
+      </Label>
+    </InputContainer>
+  );
+};
+
+

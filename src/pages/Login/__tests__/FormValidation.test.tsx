ba7842d24542d378f4b1dbe54f6d6ea51d2ad0ae
Vítor Pereira Fontes <dev.vitorpfontes002@gmail.com>
2023-09-24 11:26:12 -0300

    test(src/pages/login): criando um novo teste para a tela de login
    Criando um mock do formulário da tela de login para veriricar se este está enviando corretamente os
    dados para um futura função, que será responsável por realizar o envio desses dados para a API.

--- src/pages/Login/__tests__/FormValidation.test.tsx
+++ src/pages/Login/__tests__/FormValidation.test.tsx
@@ -2,10 +2,14 @@ import '@testing-library/jest-dom'
 import userEvent from '@testing-library/user-event'
 import 'jest-styled-components'
 import { render, screen } from 'test-utils'
-import Login from ".."
+import { FormMock } from "../__mocks__/Form"
+
+const mockLogin = jest.fn((email: string, password: string) => {
+    Promise.resolve({ email, password })
+})
 
 beforeEach(async () => {
-    render(<Login />)
+    render(<FormMock loginFunction={mockLogin} />)
 })
 
 describe('Formulário da tela de login', () => {
@@ -58,5 +62,18 @@ describe('Formulário da tela de login', () => {
         expect(screen.getByTestId('E-mail-span')).toHaveTextContent('Por favor preencha este campo.')
         expect(screen.getByTestId('Password-span')).toBeInTheDocument()
         expect(screen.getByTestId('Password-span')).toHaveTextContent('Por favor preencha este campo.')
+    })
+
+    test('Os dados estão sendo enviados do formulário para a função responsável pelo login', async () => {
+        await userEvent.type(screen.getByTestId('E-mail-input'), 'teste@gmail.com')
+        await userEvent.type(screen.getByTestId('Password-input'), 'Qualquer_coisa')
+
+        await userEvent.click(screen.getByRole('button'))
+
+        screen.queryAllByRole('span').forEach(span => {
+            expect(span).not.toBeInTheDocument()
+        })
+
+        expect(mockLogin).toBeCalledWith('teste@gmail.com', 'Qualquer_coisa')
     })
 })

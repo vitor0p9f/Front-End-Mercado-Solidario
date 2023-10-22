8b0e5862990445532f8065bd65a4e321cf5f564a
vitor0p9f <dev.vitorpfontes002@gmail.com>
2023-10-22 16:29:24 -0300

    test(src/pages/login): modificando os testes que verificam a autenticação
    Reescrevendo alguns testes para verificar se o cookie que irá armezenar o token de autenticação está
    sendo criado e populado devidamente. Refatorando os testes para separá-los por categorias.

--- src/pages/Login/__tests__/FormValidation.test.tsx
+++ src/pages/Login/__tests__/FormValidation.test.tsx
@@ -4,6 +4,7 @@ import userEvent from '@testing-library/user-event'
 import 'jest-styled-components'
 import { render, screen } from 'test-utils'
 import { FormMock } from "../__mocks__/Form"
+import Cookies from "js-cookie"
 
 const mockLogin = jest.fn(authUser)
 
@@ -11,102 +12,123 @@ beforeEach(async () => {
     render(<FormMock loginFunction={mockLogin} />)
 })
 
-describe('Formulário da tela de login', () => {
-    test('Todos os campos estão preenchidos corretamente', async () => {
-        await userEvent.type(screen.getByTestId('E-mail-input'), 'teste@gmail.com')
-        await userEvent.type(screen.getByTestId('Password-input'), 'Qualquer_coisa')
+describe('Tela de login', () => {
+    describe('Validação do formulário', () => {
+        test('Todos os campos estão preenchidos corretamente', async () => {
+            await userEvent.type(screen.getByTestId('E-mail-input'), 'teste@gmail.com')
+            await userEvent.type(screen.getByTestId('Password-input'), 'Qualquer_coisa')
 
-        await userEvent.click(screen.getByRole('button'))
+            await userEvent.click(screen.getByRole('button'))
 
-        screen.queryAllByRole('span').forEach(span => {
-            expect(span).not.toBeInTheDocument()
+            screen.queryAllByRole('span').forEach(span => {
+                expect(span).not.toBeInTheDocument()
+            })
+        })
+
+        test('O campo de E-mail não está preenchido', async () => {
+            await userEvent.type(screen.getByTestId('Password-input'), 'Qualquer_coisa')
+
+            await userEvent.click(screen.getByRole('button'))
+
+            expect(screen.getByTestId('E-mail-input')).toHaveFocus()
+            expect(screen.getByTestId('E-mail-span')).toBeInTheDocument()
+            expect(screen.getByTestId('E-mail-span')).toHaveTextContent('Por favor preencha este campo.')
+            expect(screen.queryByTestId('Password-span')).not.toBeInTheDocument()
+        })
+
+        test('O campo de E-mail está preenchido com um e-mail inválido', async () => {
+            await userEvent.type(screen.getByTestId('E-mail-input'), 'teste.123.com')
+
+            expect(screen.getByTestId('E-mail-input')).toHaveFocus()
+            expect(screen.getByTestId('E-mail-span')).toBeInTheDocument()
+            expect(screen.getByTestId('E-mail-span')).toHaveTextContent('Este endereço de e-mail é inválido!')
+        })
+
+        test('O campo Senha não está preenchido', async () => {
+            await userEvent.type(screen.getByTestId('E-mail-input'), 'teste@gmail.com')
+
+            await userEvent.click(screen.getByRole('button'))
+
+            expect(screen.getByTestId('Password-input')).toHaveFocus()
+            expect(screen.getByTestId('Password-span')).toBeInTheDocument()
+            expect(screen.getByTestId('Password-span')).toHaveTextContent('Por favor preencha este campo.')
+            expect(screen.queryByTestId('E-mail-span')).not.toBeInTheDocument()
+        })
+
+        test('O campo Senha não está preenchido', async () => {
+            await userEvent.type(screen.getByTestId('E-mail-input'), 'teste@gmail.com')
+
+            await userEvent.click(screen.getByRole('button'))
+
+            expect(screen.getByTestId('Password-input')).toHaveFocus()
+            expect(screen.getByTestId('Password-span')).toBeInTheDocument()
+            expect(screen.getByTestId('Password-span')).toHaveTextContent('Por favor preencha este campo.')
+            expect(screen.queryByTestId('E-mail-span')).not.toBeInTheDocument()
+        })
+
+        test('Nenhum dos campos foi preenchido', async () => {
+            await userEvent.click(screen.getByRole('button'))
+
+            expect(screen.getByTestId('E-mail-input')).toHaveFocus()
+            expect(screen.getByTestId('E-mail-span')).toBeInTheDocument()
+            expect(screen.getByTestId('E-mail-span')).toHaveTextContent('Por favor preencha este campo.')
+            expect(screen.getByTestId('Password-span')).toBeInTheDocument()
+            expect(screen.getByTestId('Password-span')).toHaveTextContent('Por favor preencha este campo.')
         })
     })
 
-    test('O campo de E-mail não está preenchido', async () => {
-        await userEvent.type(screen.getByTestId('Password-input'), 'Qualquer_coisa')
+    describe('Autenticação do usuário', () => {
+        test('Os dados estão sendo enviados do formulário para a função responsável pelo login', async () => {
+            await userEvent.type(screen.getByTestId('E-mail-input'), 'teste@gmail.com')
+            await userEvent.type(screen.getByTestId('Password-input'), 'Qualquer_coisa')
 
-        await userEvent.click(screen.getByRole('button'))
+            await userEvent.click(screen.getByRole('button'))
 
-        expect(screen.getByTestId('E-mail-input')).toHaveFocus()
-        expect(screen.getByTestId('E-mail-span')).toBeInTheDocument()
-        expect(screen.getByTestId('E-mail-span')).toHaveTextContent('Por favor preencha este campo.')
-        expect(screen.queryByTestId('Password-span')).not.toBeInTheDocument()
-    })
+            screen.queryAllByRole('span').forEach(span => {
+                expect(span).not.toBeInTheDocument()
+            })
 
-    test('O campo de E-mail está preenchido com um e-mail inválido', async () => {
-        await userEvent.type(screen.getByTestId('E-mail-input'), 'teste.123.com')
-
-        expect(screen.getByTestId('E-mail-input')).toHaveFocus()
-        expect(screen.getByTestId('E-mail-span')).toBeInTheDocument()
-        expect(screen.getByTestId('E-mail-span')).toHaveTextContent('Este endereço de e-mail é inválido!')
-    })
-
-    test('O campo Senha não está preenchido', async () => {
-        await userEvent.type(screen.getByTestId('E-mail-input'), 'teste@gmail.com')
-
-        await userEvent.click(screen.getByRole('button'))
-
-        expect(screen.getByTestId('Password-input')).toHaveFocus()
-        expect(screen.getByTestId('Password-span')).toBeInTheDocument()
-        expect(screen.getByTestId('Password-span')).toHaveTextContent('Por favor preencha este campo.')
-        expect(screen.queryByTestId('E-mail-span')).not.toBeInTheDocument()
-    })
-
-    test('Nenhum dos campos foi preenchido', async () => {
-        await userEvent.click(screen.getByRole('button'))
-
-        expect(screen.getByTestId('E-mail-input')).toHaveFocus()
-        expect(screen.getByTestId('E-mail-span')).toBeInTheDocument()
-        expect(screen.getByTestId('E-mail-span')).toHaveTextContent('Por favor preencha este campo.')
-        expect(screen.getByTestId('Password-span')).toBeInTheDocument()
-        expect(screen.getByTestId('Password-span')).toHaveTextContent('Por favor preencha este campo.')
-    })
-
-    test('Os dados estão sendo enviados do formulário para a função responsável pelo login', async () => {
-        await userEvent.type(screen.getByTestId('E-mail-input'), 'teste@gmail.com')
-        await userEvent.type(screen.getByTestId('Password-input'), 'Qualquer_coisa')
-
-        await userEvent.click(screen.getByRole('button'))
-
-        screen.queryAllByRole('span').forEach(span => {
-            expect(span).not.toBeInTheDocument()
+            expect(mockLogin).toBeCalledWith('teste@gmail.com', 'Qualquer_coisa')
         })
 
-        expect(mockLogin).toBeCalledWith('teste@gmail.com', 'Qualquer_coisa')
-    })
+        test('O usuário está sendo autenticado', async () => {
+            await userEvent.type(screen.getByTestId('E-mail-input'), 'teste@gmail.com')
+            await userEvent.type(screen.getByTestId('Password-input'), 'Qualquer_coisa')
 
-    test('O usuário está sendo autenticado', async () => {
-        await userEvent.type(screen.getByTestId('E-mail-input'), 'teste@gmail.com')
-        await userEvent.type(screen.getByTestId('Password-input'), 'Qualquer_coisa')
+            await userEvent.click(screen.getByRole('button'))
 
-        await userEvent.click(screen.getByRole('button'))
+            screen.queryAllByRole('span').forEach(span => {
+                expect(span).not.toBeInTheDocument()
+            })
 
-        screen.queryAllByRole('span').forEach(span => {
-            expect(span).not.toBeInTheDocument()
+            expect(mockLogin).toBeCalledWith('teste@gmail.com', 'Qualquer_coisa')
+
+            await mockLogin('teste@gmail.com', 'Qualquer_coisa')
+
+            const storagedToken = Cookies.get('token');
+
+            expect(storagedToken).toBe(process.env.TOKEN);
         })
 
-        expect(mockLogin).toBeCalledWith('teste@gmail.com', 'Qualquer_coisa')
+        test('Houve um erro durante o processo de autenticação', async () => {
+            Cookies.remove('token')
 
-        const returnedValue = await mockLogin('teste@gmail.com', 'Qualquer_coisa')
+            await userEvent.type(screen.getByTestId('E-mail-input'), 'desconhecido@gmail.com')
+            await userEvent.type(screen.getByTestId('Password-input'), 'Qualquer_coisa')
 
-        expect(returnedValue).toBe('Você se autenticou!')
-    })
+            await userEvent.click(screen.getByRole('button'))
 
-    test('Houve um erro durante o processo de autenticação', async () => {
-        await userEvent.type(screen.getByTestId('E-mail-input'), 'teste@gmail.com')
-        await userEvent.type(screen.getByTestId('Password-input'), 'Qualquer_coisa')
+            screen.queryAllByRole('span').forEach(span => {
+                expect(span).not.toBeInTheDocument()
+            })
 
-        await userEvent.click(screen.getByRole('button'))
+            expect(mockLogin).toBeCalledWith('desconhecido@gmail.com', 'Qualquer_coisa')
 
-        screen.queryAllByRole('span').forEach(span => {
-            expect(span).not.toBeInTheDocument()
+            await mockLogin('desconhecido@gmail.com', 'Qualquer_coisa')
+
+            const storagedToken = Cookies.get('token')
+
+            expect(storagedToken).toBeUndefined()
         })
-
-        expect(mockLogin).toBeCalledWith('teste@gmail.com', 'Qualquer_coisa')
-
-        const returnedValue = await mockLogin('desconhecido@gmail.com', 'Qualquer_coisa')
-
-        expect(returnedValue).toBe('Parece que ocorreu um erro')
     })
 })

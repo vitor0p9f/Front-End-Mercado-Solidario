ae3bb1ad63065487f9883d242e52c6ca873b498a
vitor0p9f <dev.vitorpfontes002@gmail.com>
2023-11-19 16:30:51 -0300

    style(src/styles): adicionando a opção de padding horizintal para o container de página

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,5 +1,17 @@
 import { createGlobalStyle, styled } from "styled-components";
 
+const sizes = {
+    mobile: '430px',
+    tablet: '768px',
+    laptop: '1024px',
+};
+
+export const devices = {
+    mobile: `(min-width: ${sizes.mobile})`,
+    tablet: `(min-width: ${sizes.tablet})`,
+    laptop: `(min-width: ${sizes.laptop})`,
+};
+
 export const GlobalStyle = createGlobalStyle`
 *{
     margin: 0;
@@ -23,24 +35,23 @@ body, input, textarea, button {
 }
 `;
 
-export const PageContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 100vw;
-  height: 100vh;
-  background-color: ${props => props.theme.colors.white};
+type PageContainerProps = {
+    $horizontalPadding?: number
+}
+
+export const PageContainer = styled.div<PageContainerProps>`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+    background-color: ${props => props.theme.colors.white};
+    padding-left: ${props => (props.$horizontalPadding ? (props.$horizontalPadding - 1) : 0)}rem;
+    padding-right: ${props => (props.$horizontalPadding ? (props.$horizontalPadding - 1) : 0)}rem;
+
+    @media ${devices.laptop} {
+        padding-left: ${props => (props.$horizontalPadding || 0)}rem;
+        padding-right: ${props => (props.$horizontalPadding || 0)}rem;
+     }
 `;
-
-const sizes = {
-    mobile: '430px',
-    tablet: '768px',
-    laptop: '1024px',
-};
-
-export const devices = {
-    mobile: `(min-width: ${sizes.mobile})`,
-    tablet: `(min-width: ${sizes.tablet})`,
-    laptop: `(min-width: ${sizes.laptop})`,
-};
